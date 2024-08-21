d5a26102842e2ee8b030b5559dbbbec62863bfeb
Florian Gleixner <Florian.Gleixner@lrz.de>
2024-08-22 00:09:00 +0200

    Fix Symfony 7.1 deprecation warnings

--- src/Bundle/DependencyInjection/SineflowClamAVExtension.php
+++ src/Bundle/DependencyInjection/SineflowClamAVExtension.php
@@ -2,14 +2,14 @@
 
 namespace Sineflow\ClamAV\Bundle\DependencyInjection;
 
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 
 class SineflowClamAVExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container) : void
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);

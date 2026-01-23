ae3f15490b687ec2fcc86d8a14615082958dfe34
Leah <leah.venema@bakerware.nl>
2026-01-23 10:41:21 +0100

    Fix Tests

--- tests/Functional/ScannerTest.php
+++ tests/Functional/ScannerTest.php
@@ -111,7 +111,7 @@ class ScannerTest extends TestCase
             [realpath(__DIR__.'/../Files/clean.txt'), false, ''],
             [realpath(__DIR__.'/../Files/eicar.txt'), true, 'Win.Test.EICAR_HDB-1'],
             [realpath(__DIR__.'/../Files/eicar-dropper.pdf'), true, 'Pdf.Dropper.Agent-6299400-0'],
-            [realpath(__DIR__.'/../Files/infected-archive.zip'), true, 'Win.Test.EICAR_HDB-1'],
+            [realpath(__DIR__.'/../Files/infected-archive.zip'), true, 'Eicar-Test-Signature'],
         ];
     }
 
